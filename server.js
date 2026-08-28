a9a9b5395bc3eb66805e7215ab744b7436ea9070
moongrammarschooledu-star <moongrammarschool.edu@gmail.com>
2026-08-28 14:40:36 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ const dashboardRoutes = require('./routes/dashboard');
 const studentRoutes = require('./routes/students');
 const promotionRoutes = require('./routes/promotions');
 const settingRoutes = require('./routes/settings');
-const adminMigrateRoutes = require('./routes/adminMigrate');
 
 const app = express();
 
@@ -43,7 +42,6 @@ app.use('/api/dashboard', dashboardRoutes);
 app.use('/api/students', studentRoutes);
 app.use('/api/promotions', promotionRoutes);
 app.use('/api/settings', settingRoutes);
-app.use('/api/admin/migrate', adminMigrateRoutes);
 
 app.use('/api', notFound);
 app.use(errorHandler);
